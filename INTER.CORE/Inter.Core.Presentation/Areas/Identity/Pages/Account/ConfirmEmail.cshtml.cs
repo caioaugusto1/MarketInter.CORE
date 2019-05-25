292a50b1018b5953a9fb1557d3c94a2a3f283a38
Caio Augusto <caiio_augustto@yahoo.com.br>
2019-05-25 16:33:05 +0100

    bootstrap implementation

--- INTER.CORE/Inter.Core.Presentation/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ INTER.CORE/Inter.Core.Presentation/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Inter.Core.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,9 +13,9 @@ namespace Inter.Core.Presentation.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class ConfirmEmailModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ConfirmEmailModel(UserManager<IdentityUser> userManager)
+        public ConfirmEmailModel(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
         }
